5d3425967349a02ce40ee606cffa86f83a25aea0
Shannon Li <113462470+ShannonnLi@users.noreply.github.com>
2024-05-20 21:04:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const urls = [
     "https://i.pinimg.com/originals/1e/31/51/1e31515208a1772c6cfea85d1071474e.jpg",
+    "https://www.etsy.com/listing/1199240838/colorful-crochet-necktie-retro-knit-tie?gpla=1&gao=1&&utm_source=google&utm_medium=cpc&utm_campaign=shopping_us_a-accessories-suit_and_tie_accessories-neckties&utm_custom1=_k_EAIaIQobChMIloG7qMOdhgMVc0f_AR0h4A5jEAQYAyABEgKorfD_BwE_k_&utm_content=go_12573072685_122305127929_507558052015_pla-355209283428_c__1199240838_556277119&utm_custom2=12573072685&gad_source=1&gclid=EAIaIQobChMIloG7qMOdhgMVc0f_AR0h4A5jEAQYAyABEgKorfD_BwE",
+   "https://fivevoltlogic.biz/products/functional-light-up-mini-tiffany-lamp-earrings-stained-glass-lamp-earrings-working-lamp-earrings-replica-tiffany-lamp-moth-meme",
+    "https://farmrio.com/products/beige-rose-shaped-bodice-sleeveless-maxi-dress?variant=41537224245341",
+    "https://www.dollskill.com/products/its-always-greener-platform-sandals?epik=dj0yJnU9eXZhaFprbnhSUW5vYW56b004NjlmZlpvNVM1SW5Id0EmcD0wJm49STVoajMzc2tjbFJGN2NpbEo4bHFTZyZ0PUFBQUFBR1pMOEtv",
+    "https://sockseason.com/products/la-fleur-sheer-floral-jacquard-mesh-crew-sock-irises",
+    "https://ph5.com/collections/dresses/products/mia-signature-wavy-plaid-dress-in-eco-yarn-2?variant=40880400072873",
+    "https://sockcandy.com/products/y2k-hearts-ruffle-sheer-crew-sock?epik=dj0yJnU9MVV6cXVFVWR0Z1dCMzR4ZF95c242S1NlbVZnTVgzWU0mcD0wJm49ak1iM1pzTzMzTEYzVjFfOGNRN0N4USZ0PUFBQUFBR1pMOGRj",
+    "https://orchardmile.com/sock-candy/heart-floral-sheer-crew-sock-jfbcce75fa9",
+    
+    
     // Add more new URLs as needed
 ];
 
